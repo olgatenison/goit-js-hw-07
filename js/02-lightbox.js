26f488ad4b9adee32826f261b87ebbdd7af5ebda
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-09-26 22:54:48 +0300

    summary

---
+++ js/02-lightbox.js
@@ -0,0 +1,24 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+// function createMarkup(arr) {
+//   return arr
+//     .map(
+//       ({ id, img, name, price }) => `
+//   <li data-product-id="${id}" class="item js-product-item">
+//     <img src="${img}" alt="${name}" width="300">
+//     <h2>${name}</h2>
+//     <p>Ціна: ${price}грн</p>
+//  </li>
+//   `
+//     )
+//     .join("");
+// }
+
+// function handlerClick(evt) {
+//   if (evt.target === evt.currentTarget) {
+//     return;
+//   }
+//   // console.log(evt.target);
