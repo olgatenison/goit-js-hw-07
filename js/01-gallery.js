ac8ab3e00a655b6e55a6b847a80aeebbd199384d
olgatenison <88651744+olgatenison@users.noreply.github.com>
2023-09-29 15:35:47 +0300

    2 tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,58 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// отримуємо масив карток
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+//створюємо всі картки
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      
+      <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+      />
+      
+    </a>
+  </li>`;
+  })
+  .join("");
+
+// вставляємо в хтмл
+gallery.insertAdjacentHTML("beforeend", markup);
+
+// вішаємо клік
+gallery.addEventListener("click", onGallery);
+
+function onGallery(evt) {
+  // прибераємо поведінку за замовчуванням
+  evt.preventDefault();
+
+  // перевіряем куди клікнули
+  if (evt.target.className !== "gallery__image") {
+    return;
+  } else {
+    // передаємо в модалку адресу картинки з target.dataset
+    const fullImgModal = basicLightbox.create(`
+    <div class="modal">
+    <img src=${evt.target.dataset.source} 
+    width="800" height="600">
+    </div>
+    `);
+    // викликаємо  модалку
+    fullImgModal.show();
+
+    // слухаємо клавіатуру
+    window.addEventListener("keydown", onModalKeydown);
+    // закриття модалки по Escape
+    function onModalKeydown(evt) {
+      if (evt.code === "Escape") {
+        fullImgModal.close();
+      }
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,24 +1,31 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
 
-// function createMarkup(arr) {
-//   return arr
-//     .map(
-//       ({ id, img, name, price }) => `
-//   <li data-product-id="${id}" class="item js-product-item">
-//     <img src="${img}" alt="${name}" width="300">
-//     <h2>${name}</h2>
-//     <p>Ціна: ${price}грн</p>
-//  </li>
-//   `
-//     )
-//     .join("");
-// }
+//створюємо всі картки
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      
+      <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+      />
+      
+    </a>
+  </li>`;
+  })
+  .join("");
 
-// function handlerClick(evt) {
-//   if (evt.target === evt.currentTarget) {
-//     return;
-//   }
-//   // console.log(evt.target);
+// вставляємо в хтмл
+gallery.insertAdjacentHTML("beforeend", markup);
+
+// підключаємо галерею
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionPosition: "bottom",
+  captionsData: "alt",
+  captionDelay: 250,
+});
